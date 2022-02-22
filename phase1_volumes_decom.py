e2134a45b8b60e941d196141669bef591c9fe6e0
babusmahadb <reachbabuinbox@gmail.com>
2022-02-22 08:27:29 +0000

    CIFS deletion included

--- phase1_volumes_decom.py
+++ phase1_volumes_decom.py
@@ -15,6 +15,7 @@ from getpass import getpass
 import logging
 import requests
 import os
+import sys
 ur.disable_warnings()
 
 
@@ -175,7 +176,52 @@ class bcolors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+  
+
+def cifs_delete(cluster: str,vol_name: str,svm_name: str,cifs_list: str, headers_inc: str):
     
+    """ Delete cifs shares associated with volume """
+    
+    svm_url="https://{}/api/svm/svms?name={}".format(cluster,svm_name)
+    response = requests.get(svm_url, headers=headers_inc, verify=False)
+    svm_json = response.json()
+    
+    svm_dt = dict(svm_json)
+    svm_rd = svm_dt['records']
+    
+    for i in svm_rd:
+        svm_uuid = i['uuid']
+    
+    clist = cifs_list.replace("'","").strip('][').split(', ')
+    
+    for share in clist:
+        
+        if "root" in share:
+            print("cifs share "+bcolors.OKBLUE +share+bcolors.ENDC +" is Root Volume")
+        else:
+            print()
+            check = input("Do you want to delete cifs share named:"+bcolors.OKBLUE+share+bcolors.ENDC+"(y/n)")
+            if check =='y':
+                cifs_del_url = "https://{}/api/protocols/cifs/shares/{}/{}?return_timeout=5".format(cluster,svm_uuid,share)
+                                
+                try:
+                    response = requests.delete(cifs_del_url, headers=headers_inc, verify=False)
+                except requests.exceptions.HTTPError as err:
+                    print(str(err))
+                    sys.exit(1)
+                except requests.exceptions.RequestException as err:
+                    print(str(err))
+                    sys.exit(1)
+                cifs_del_json = response.json()
+                #print(cifs_del_json)
+                if 'error' in cifs_del_json:
+                    print(cifs_del_json)
+                    sys.exit(1)
+                else:
+                                        
+                    if cifs_del_json == {}:
+                        print("Deletion of Share name "+bcolors.OKBLUE +share+bcolors.ENDC +" of Volume:"+bcolors.OKBLUE +vol_name+bcolors.ENDC +" is "+bcolors.OKGREEN +"Successful"+ bcolors.ENDC +".") 
+                        
 if __name__ == "__main__":
 
     logging.basicConfig(
@@ -207,9 +253,18 @@ if __name__ == "__main__":
         svm_name = vol_df['Vserver Name'][ind]
         
         print("############################"+bcolors.OKCYAN+vol_name+bcolors.ENDC +"########################################################")
-        
+        print()
        # print(bcolors.WARNING+ vol_name +bcolors.ENDC)
-
+        cifs_check = vol_df['No. of CIFS Shares'][ind]
+        
+        if cifs_check!=0:
+            cifs_list = vol_df['CIFS Shares List'][ind]
+            print("Volume "+bcolors.OKBLUE+vol_name+bcolors.ENDC +" has CIFS share(s) "+bcolors.OKBLUE+cifs_list+bcolors.ENDC +".")
+            cifs_delete(cls_name,vol_name,svm_name,cifs_list, headers)
+        else:
+            print("Volume "+bcolors.OKBLUE+vol_name+bcolors.ENDC +" not configured for CIFS.")
+            
+        print()    
         snpmir_check = vol_df['SnapMirror(Y/N)'][ind]
         if snpmir_check == 'Yes':
             #print()
@@ -219,7 +274,9 @@ if __name__ == "__main__":
         else:
             #print()
             print("Volume "+bcolors.OKBLUE+vol_name+bcolors.ENDC +" of Cluster/Vserver :"+bcolors.HEADER+cls_name+"/"+svm_name+bcolors.ENDC +" does not have snapmirror configured")
-        print("###########################################################################################################################")    
+            
+        print()    
+        #print("###########################################################################################################################")    
     #print(vol_df.loc[vol_df['SnapMirror(Y/N)']])
     ''#print(vol_df.T)
 
